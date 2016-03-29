3ec86b995984e8ad356cb96ad0d51e0c8ea6ba95
TJ VanToll <tj.vantoll@gmail.com>
2016-03-29 10:32:35 -0400

    Removing LiveSync patch

--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,6 @@ import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {HTTP_PROVIDERS} from "angular2/http";
 import {NS_ROUTER_PROVIDERS} from "nativescript-angular/router";
 import {AppComponent} from "./app.component";
-import "./livesync-patch";
 import {setStatusBarColors} from "./utils/status-bar-util";
 
 setStatusBarColors();
