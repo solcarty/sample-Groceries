112165dc28b01ae00c3f9609575ff0dba916681f
TJ VanToll <tj.vantoll@gmail.com>
2016-09-13 10:36:57 -0400

    Adding the list route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from "@angular/router";
 
 import { LoginComponent } from "./pages/login/login.component";
+import { ListComponent } from "./pages/list/list.component";
 
 export const appRoutes: Routes = [
-  { path: "", component: LoginComponent }
+  { path: "", component: LoginComponent },
+  { path: "list", component: ListComponent }
 ];
