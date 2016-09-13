f353fac3b43d3cd3e513a35905e0f93b8598224c
TJ VanToll <tj.vantoll@gmail.com>
2016-09-13 09:56:03 -0400

    Include a few more imports

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,7 @@
 import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
 import { NgModule } from "@angular/core";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 import { appRoutes } from "./app.routes";
@@ -14,6 +16,8 @@ setStatusBarColors();
 @NgModule({
   imports: [
     NativeScriptModule,
+    NativeScriptFormsModule,
+    NativeScriptHttpModule,
     NativeScriptRouterModule,
     NativeScriptRouterModule.forRoot(appRoutes)
   ],
