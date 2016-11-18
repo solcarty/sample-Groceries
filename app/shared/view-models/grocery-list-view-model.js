e75e48de549b632af3e379717b2bffbab5e9971c
TJ VanToll <tj.vantoll@gmail.com>
2016-11-18 09:56:25 -0500

    Updating starting point to updated theme

--- app/shared/view-models/grocery-list-view-model.js
+++ app/shared/view-models/grocery-list-view-model.js
@@ -3,19 +3,19 @@ var fetchModule = require("fetch");
 var ObservableArray = require("data/observable-array").ObservableArray;
 
 function GroceryListViewModel(items) {
-    var viewModel = new ObservableArray(items);
+  var viewModel = new ObservableArray(items);
 
 
 
-    return viewModel;
+  return viewModel;
 }
 
 function handleErrors(response) {
-    if (!response.ok) {
-        console.log(JSON.stringify(response));
-        throw Error(response.statusText);
-    }
-    return response;
+  if (!response.ok) {
+    console.log(JSON.stringify(response));
+    throw Error(response.statusText);
+  }
+  return response;
 }
 
 module.exports = GroceryListViewModel;
